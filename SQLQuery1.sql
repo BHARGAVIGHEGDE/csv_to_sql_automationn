5ecf1f2b54a31de1f58000b305c32bb7b5a4f772
bhargavi <bhargavihegde2002@gmail.com>
2025-05-11 21:32:41 +0530

    sql page

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+create database abc
+use abc;
+
+create table trialone(column1 varchar(20),column2 varchar(20))
+create table trialtwo(column3 varchar(20),column4 varchar(20))
+select *from trialone
+select *from trialtwo
